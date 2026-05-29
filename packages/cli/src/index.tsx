b5861a130b3aa1fdf4b4fda8596b28322c19df33
learner-Adarsh <ashuyadav290405@gmail.com>
2026-05-30 01:12:22 +0530

    01: project setup and component architecture

---
+++ packages/cli/src/index.tsx
@@ -0,0 +1,25 @@
+import { createCliRenderer} from "@opentui/core";
+import { createRoot } from "@opentui/react";
+import { Header } from "./components/header";
+import { InputBar } from "./components/input-bar";
+
+
+
+
+
+function App() {
+  return (
+    <box alignItems="center" justifyContent="center" backgroundColor="black" height="100%" width="100%" gap={2}>
+      <Header />
+      <box width="100%" maxWidth={78} paddingX={2}>
+        <InputBar onSubmit={() => {}} />
+      </box>
+    </box>
+  );
+}
+
+const renderer = await createCliRenderer({
+  targetFps: 60,
+  exitOnCtrlC: false,
+});
+createRoot(renderer).render(<App />);
